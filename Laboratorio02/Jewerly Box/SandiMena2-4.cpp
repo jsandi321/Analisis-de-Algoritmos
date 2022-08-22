6ac5563a479ad4397148d097f229fe04adb35eae
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-08-22 13:38:30 -0600

    Ejercicios a resolver Laboratorio II

--- Laboratorio02/Jewerly Box/SandiMena2-4.cpp
+++ Laboratorio02/Jewerly Box/SandiMena2-4.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
-
 /*
     Jeison Sandí Mena
     UVa 11876 N + NOD (N)
     Complejidad: 
 */
-
 int main(int argc, char const *argv[])
 {
-    
+    /* code */
     return 0;
 }
