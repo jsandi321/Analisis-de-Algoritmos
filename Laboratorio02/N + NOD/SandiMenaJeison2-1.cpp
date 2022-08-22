3f663f08abc80d625753760907a7180ec7b73016
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-08-22 14:08:08 -0600

    Correccion en ejercicio 01 N + NOD usando sqrt para hacer menos iteraciones por cada valor de entrada en NOD

--- Laboratorio02/N + NOD/SandiMenaJeison2-1.cpp
+++ Laboratorio02/N + NOD/SandiMenaJeison2-1.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 /*
     Jeison Sandí Mena
     UVa 11876 N + NOD (N)
     Complejidad: 
-
 */
+//Calcular NOD
+
+int NOD(int n){
+    int factores = 1;
+    for(int i = 1; i < sqrt(n); i++){
+        if(n%i == 0){
+            factores ++;
+            factores ++;
+        }
+        else{
+            if(i*i == n){
+        	factores ++;
+            }
+        }
+    }
+    return factores;
+}
+//Secuencia
 int main(int argc, char const *argv[])
 {
     /* code */
+    int testing = NOD(100);
+    cout<<testing;
     return 0;
 }
