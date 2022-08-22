78013bf128047932c533ed91d527807cc84dd48d
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-08-22 13:32:28 -0600

    Ejercicios a resolver Laboratorio II

---
+++ Laboratorio02/Angry Bids/SandiMenaJeison2-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*
+    Jeison Sandí Mena
+    UVa 12965 Angry Bids
+    Complejidad: 
+
+*/
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ Laboratorio02/Bit Maps/SandiMenaJeison2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Jeison Sandí Mena
+    UVa 11876 N + NOD (N)
+    Complejidad: 
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
