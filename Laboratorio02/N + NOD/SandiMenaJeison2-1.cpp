20a2f15f43a571af89354750f118904405b798b2
jsandi321 <jsandi321@gmail.com>
2022-08-24 15:29:36 -0600

    Calculo de Secuencia principañ

--- Laboratorio02/N + NOD/SandiMenaJeison2-1.cpp
+++ Laboratorio02/N + NOD/SandiMenaJeison2-1.cpp
@@ -6,6 +6,7 @@ using namespace std;
     UVa 11876 N + NOD (N)
     Complejidad: 
 */
+
 //Calcular NOD
 long int NOD(int n){
     //Complejidad: O(n) = sqrt(n)
@@ -23,11 +24,28 @@ long int NOD(int n){
     return divisores;
 }
 
+void calcularSecuencia(int max){
+	long int V[max+1];
+	V[0] = 1;
+	//int inicio = A;
+	for(int i = 1; i <= max; i++){
+		V[i] = V[i-1] + NOD(V[i-1]);
+		cout << V[i] << "\n";
+	}
+}
+
 //Secuencia
 int main(int argc, char const *argv[])
 {
-    /* code */
-    long int testing = NOD(100);
-    cout<<testing;
+	int T, A, B;
+	cin >> T;
+	for(int i = 1; i <= T; i++){
+		cin >> A;
+		cin >> B;
+		
+		calcularSecuencia(9999);
+		cout << "Secuencia Calculada";
+		
+	}
     return 0;
 }
