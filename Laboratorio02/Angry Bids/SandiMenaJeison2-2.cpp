2e760cc52bcba70ffeac3eec2c9eb1670ddfcdb3
jsandi321 <jsandi321@gmail.com>
2022-08-24 23:41:00 -0600

    "Avance ejercicio 2 AngryBids lectura y ordenamiento de arreglos"

--- Laboratorio02/Angry Bids/SandiMenaJeison2-2.cpp
+++ Laboratorio02/Angry Bids/SandiMenaJeison2-2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 /*
     Jeison Sandí Mena
@@ -6,8 +8,33 @@ using namespace std;
     Complejidad: 
 
 */
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+
+
+int main() {
+    //Lectura de datos
+	int T, P, C;
+	cin >> T;
+	for(int i = 0; i < T; i++){
+		cin >> P;
+		cin >> C;
+		int preciosP[P];
+		int preciosC[C];
+		for(int i = 0; i < P; i++){
+			int precioActual;
+			cin >> precioActual;
+			preciosP[i] = precioActual;
+		}
+		for(int i = 0; i < C; i++){
+			int precioActual;
+			cin >> precioActual;
+			preciosC[i] = precioActual;
+		}
+
+        //Ordenamiento de arreglos
+        sort(preciosP, preciosP + sizeof preciosP/sizeof preciosP[0]);
+        sort(preciosC, preciosC + sizeof preciosC/sizeof preciosC[0]);
+        
+
+	}
+	return 0;
 }
