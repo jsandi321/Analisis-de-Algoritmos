3dc934fb317df1900f04a4743e3b19ad23074b5c
jsandi321 <jsandi321@gmail.com>
2022-08-25 23:03:50 -0600

    Implementacion de JewerlyBox

--- Laboratorio02/Jewerly Box/SandiMena2-4.cpp
+++ Laboratorio02/Jewerly Box/SandiMena2-4.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 /*
     Jeison Sandí Mena
-    UVa 11876 N + NOD (N)
+    Kattis JewerlyBox
     Complejidad: 
 */
+
+double jewerlyBox(int x, int y){
+	double h;
+	h = (x+y - sqrt((x+y)*(x+y) - 3 * x * y))/6;
+	double volumenMax = (x-2*h)* (y-2*h) * h;
+	return volumenMax;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    //Lectura de datos y llamadas
+    int inputs, x, y;
+    cin >> inputs;
+    for(int i = 0; i < inputs; i++){
+        cin >> x;
+        cin >> y;
+        double volumenMax = jewerlyBox(x, y);
+        cout << volumenMax << "\n";
+    }
     return 0;
 }
