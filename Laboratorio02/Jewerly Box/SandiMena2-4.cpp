e27bcba44fd2f2bd16038bc881cc2746ec2c2ced
jsandi321 <jsandi321@gmail.com>
2022-08-25 23:16:55 -0600

    Jewerly Box Completo

--- Laboratorio02/Jewerly Box/SandiMena2-4.cpp
+++ Laboratorio02/Jewerly Box/SandiMena2-4.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 /*
     Jeison Sandí Mena
     Kattis JewerlyBox
-    Complejidad: 
+    Complejidad: O(inputs)
 */
 
 double jewerlyBox(int x, int y){
+    //O(1)
 	double h;
 	h = (x+y - sqrt((x+y)*(x+y) - 3 * x * y))/6;
 	double volumenMax = (x-2*h)* (y-2*h) * h;
@@ -19,11 +21,11 @@ int main(int argc, char const *argv[])
     //Lectura de datos y llamadas
     int inputs, x, y;
     cin >> inputs;
-    for(int i = 0; i < inputs; i++){
+    for(int i = 0; i < inputs; i++){    //O(inputs)
         cin >> x;
         cin >> y;
         double volumenMax = jewerlyBox(x, y);
-        cout << volumenMax << "\n";
+        cout << fixed << setprecision(11) << volumenMax << "\n";
     }
     return 0;
 }
